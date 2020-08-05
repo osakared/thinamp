1918d27ccec8331ecaee585af184c8e49d22b372
Thomas J. Webb <thomas@osakared.com>
2020-08-05 16:11:12 -0700

    Now using my bmp fixes to format

--- src/BMP.hx
+++ src/BMP.hx
@@ -1,9 +1,11 @@
 package;
 
 import format.bmp.Reader;
+import format.bmp.Tools;
 import haxe.io.Bytes;
 import haxe.io.BytesInput;
 import lime.graphics.Image;
+import lime.math.Rectangle;
 
 class BMP
 {   
@@ -13,61 +15,8 @@ class BMP
         var reader = new Reader(input);
         var data = reader.read();
 
-        var image = new Image(null, 0, 0, data.header.width, data.header.height, 0xff);
-        input = new BytesInput(data.pixels);
-        var padding = Std.int(data.header.paddedStride - ((data.header.bpp * data.header.width) / 32) * 4);
-        if (data.header.bpp <= 8) {
-            var colorTable = new Array<Int>();
-            var colorTableInput = new BytesInput(data.colorTable);
-            for (_ in 0...Std.int(data.colorTable.length / 4)) {
-                colorTable.push(colorTableInput.readInt24() << 8);
-                colorTableInput.readByte();
-            }
-            var x = 0;
-            var y = data.header.topToBottom ? 0 : data.header.height - 1;
-            while (input.length > input.position) {
-                var count = input.readByte();
-                var index = input.readByte();
-                if (count == 0) {
-                    if (index == 0) {
-                        x = 0;
-                        y += data.header.topToBottom ? 1 : -1;
-                    } else if (index == 1) {
-                        break;
-                    } else if (index == 2) {
-                        x += input.readByte();
-                        y += input.readByte();
-                    } else {
-                        count = index;
-                        for (i in 0...count) {
-                            index = input.readByte();
-                            image.setPixel(x + i, y, colorTable[index], RGBA32);
-                        }
-                        if (input.position % 2 != 0) input.readByte();
-                        x += count;
-                    }
-                } else {
-                    for (i in 0...count) {
-                        image.setPixel(x + i, y, colorTable[index], RGBA32);
-                    }
-                    x += count;
-                }
-            }
-        } else {
-            for (iterY in 0...data.header.height) {
-                var y = data.header.topToBottom ? iterY : data.header.height - iterY - 1;
-                for (x in 0...data.header.width) {
-                    if (data.header.bpp == 16) {
-                        image.setPixel(x, y, input.readInt24() << 8, RGBA32);
-                    } else if (data.header.bpp == 24) {
-                        image.setPixel(x, y, input.readInt24() << 8, RGBA32);
-                    } else if (data.header.bpp == 32) {
-                        image.setPixel(x, y, input.readInt32(), RGBA32);
-                    }
-                }
-                input.read(padding);
-            }
-        }
+        var image = new Image(null, 0, 0, data.header.width, data.header.height);
+        image.setPixels(new Rectangle(0, 0, data.header.width, data.header.height), Tools.extractARGB(data), ARGB32, BIG_ENDIAN);
         return image;
     }
 }
